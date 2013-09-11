ef56232f6f34e6473a108e1348413710dd6e8272
Elijah meeks <elijahmeeks@gmail.com>
2013-09-11 09:23:02 -0700

    Meta-Nodes
    And column types are sub-ordered by timestamp.

--- bloomcase_layout.js
+++ bloomcase_layout.js
@@ -3,8 +3,23 @@ d3_layout_bloomcase = function() {
     var topLayer = 0, bottomLayer = 0, eventRelations = [], temporalProjection, eventHash = {}, tlWidth = 100, tlHeight = 100, temporalRange =[0,1], temporalType = "date", timeScale = 1, featureCollection, temporalPeriods = [], temporalEvents = [], timeSegments, numLanes = 1, earliestDate = 1, latestDate = 10;
 
     var firstNodes=[], lastNodes=[], numColumns = 1, blNodes = [], blLinks = [], columnStep = 30, blNodeHash = {};
-    var shapeHierarchy = ["Draft", "Final", "Sketch","Idea","Inspiration", "last"];
-//    var shapeHierarchy = ["last", "Inspiration", "Idea", "Sketch", "Final", "Draft", "last"];
+    var shapeHierarchy = ["Draft", "Final", "Sketch","Idea","Inspiration", "last", "meta"];
+    shapeMeasures = {
+	    Inspiration: {myWidth: 27, myHeight: 27, rank: 1,
+	    pathd: "M24.248,26.755H2.833c-1.404,0-2.548-1.143-2.548-2.548V2.793c0-1.405,1.144-2.548,2.548-2.548h21.415 c1.405,0,2.549,1.143,2.549,2.548v21.414C26.796,25.612,25.653,26.755,24.248,26.755z"                            },
+	    Final: {myWidth: 47.4, myHeight: 47.4, rank: 4,
+	    pathd: "M47.392,44.295c0,1.709-1.388,3.096-3.097,3.096H3.096C1.386,47.391,0,46.004,0,44.295V3.096 C0,1.386,1.386,0,3.096,0h41.199c1.709,0,3.097,1.386,3.097,3.096V44.295z"
+	    },
+	    Draft: {myWidth: 35, myHeight: 34.8, rank: 5,
+            pathd: "M31.42,34.758H3.341C1.5,34.758,0,33.26,0,31.419V3.341C0,1.499,1.5,0,3.341,0H31.42 c1.843,0,3.342,1.499,3.342,3.341v28.079C34.762,33.26,33.263,34.758,31.42,34.758z"
+	    },
+	    Sketch: {myWidth: 26.5, myHeight: 26.5, rank: 3,
+	    pathd: "M23.963,26.51H2.548C1.143,26.51,0,25.367,0,23.962V2.548C0,1.143,1.143,0,2.548,0h21.415 c1.405,0,2.549,1.143,2.549,2.548v21.414C26.511,25.367,25.368,26.51,23.963,26.51z"
+	    },
+	    Idea: {myWidth: 33.8, myHeight: 38.6, rank: 2,
+	    pathd: "M16.897,0C14.483,7.241,7.241,16.896,0,19.311c7.241,2.414,14.483,12.069,16.897,19.312 c2.414-7.241,9.654-16.896,16.896-19.312C26.551,16.896,19.311,7.241,16.897,0z"
+	    }
+	}
 
     
     this.nodes = function(newNodes) {
@@ -37,8 +52,11 @@ d3_layout_bloomcase = function() {
     function processNodes(incNodes) {
         for (incNode in incNodes) {
             if (incNodes[incNode]) {
+                var tArr = incNodes[incNode].timestamp.split("-");
+                incNodes[incNode].datetime = new Date(tArr[0], tArr[1], tArr[2], tArr[3],tArr[4],tArr[5]);
                 incNodes[incNode].column = -1;
-                incNodes[incNode].row = -1;                
+                incNodes[incNode].row = -1;
+                incNodes[incNode].isMeta = false;                
                 incNodes[incNode].evolvedFromArray = [];
                 blNodeHash[incNodes[incNode].nid] = incNodes[incNode];
                 //Find any nodes that didn't evolve from other nodes to make the first pass at finding the first nodes
@@ -55,15 +73,32 @@ d3_layout_bloomcase = function() {
     function createLinks(incNodes) {
         for (incNode in incNodes) {
             if (incNodes[incNode]) {
+                if (incNodes[incNode].kind == "meta") {
+                    return;
+                }
                 if (incNodes[incNode].evolvedFrom) {
                     var linkArray = incNodes[incNode].evolvedFrom.split(",");
                     for (l in linkArray) {
-                        var linkObject = {target: {}, source: {}}
+                        var linkObject1 = {target: {}, source: {}}
+                        var linkObject2 = {target: {}, source: {}}
+                        var metaNodeObject = {};
                         if(linkArray[l]) {
-                        linkObject.source = blNodeHash[linkArray[l]];
-                        linkObject.target = blNodeHash[incNodes[incNode].nid];
-                        blLinks.push(linkObject);
-                        incNodes[incNode].evolvedFromArray.push(blNodeHash[linkArray[l]]);
+                        if (blNodeHash["meta" + incNodes[incNode].nid]) {
+                            metaNodeObject = blNodeHash["meta" + incNodes[incNode].nid]
+                        }
+                        else {
+                            metaNodeObject = {kind: blNodeHash[incNodes[incNode].nid].kind, datetime: blNodeHash[incNodes[incNode].nid].datetime, row: -1, column: -1, isMeta: true, nid: "meta" + incNodes[incNode].nid, evolvedFromArray: []}
+                        blNodes.push(metaNodeObject);
+                        incNodes[incNode].evolvedFromArray.push(metaNodeObject);
+                            blNodeHash["meta" + incNodes[incNode].nid] = metaNodeObject;
+                        }
+                        linkObject1.source = blNodeHash[linkArray[l]];
+                        linkObject1.target = metaNodeObject;
+                        linkObject2.source = metaNodeObject;
+                        linkObject2.target = blNodeHash[incNodes[incNode].nid];
+                        metaNodeObject.evolvedFromArray.push(blNodeHash[linkArray[l]]);
+                        blLinks.push(linkObject1);
+                        blLinks.push(linkObject2);
                         }
                     }
                 }
@@ -74,25 +109,18 @@ d3_layout_bloomcase = function() {
     function hierarchicalLayout() {
         
         //Walk backward to find the lowest possible value
-        for (blNode in blNodes) {
-            var noMoreNodes = false;
-            var remainingNodes = blNodes[blNode].evolvedFromArray.slice();
-            while (noMoreNodes == false) {
-                if (remainingNodes.length == 0) {
-                    noMoreNodes = true;
-                }
-                else {
-                    remainingNodes[remainingNodes.length - 1].column = blNodes[blNode].column - 1;
-                    remainingNodes.pop();
-                }
+        for (blLink in blLinks) {
+            blLinks[blLink].target.column--;
+            if(blLinks[blLink].source.nid == "1") {
+                blLinks[blLink].source.column++;
             }
         }
         
             //Now sort the nodes by placeholder column to the layout starts with the earliest discovered node    
     blNodes.sort(function (a,b) {
-    if (a.column > b.column)
-    return 1;
     if (a.column < b.column)
+    return 1;
+    if (a.column > b.column)
     return -1;
     return 0;
     });
@@ -101,16 +129,12 @@ d3_layout_bloomcase = function() {
     blNodes[0].column = 2;
 
     //Step through the array again starting
-    
-    var nodesToBePositioned = 10000;
-
     var newLinks = [];
     for (z in blLinks) {
         if(blLinks[z]) {
             newLinks.push(blLinks[z])
         }
     }
-    var countdown = 100;
     while (newLinks.length > 0) {
         for (l in newLinks) {
             if (newLinks[l]) {
@@ -149,6 +173,21 @@ d3_layout_bloomcase = function() {
         }
     }
 
+    for (col in columnsByRows) {
+    columnsByRows[col].sort(function (a,b) {
+    var sortReturn = 0;
+    if (shapeMeasures[a.kind]["rank"] < shapeMeasures[b.kind]["rank"])
+    return 1;
+    if (shapeMeasures[a.kind]["rank"] > shapeMeasures[b.kind]["rank"])
+    return -1;
+    if (a.datetime < b.datetime)
+    return 1;
+    if (a.datetime > b.datetime)
+    return -1;
+    return 0;
+    });
+    }
+    
     exposedCols = columnsByRows;
             var currentKind = "";
         var currentList = [];
@@ -179,13 +218,10 @@ d3_layout_bloomcase = function() {
                 if (isFirst == true) {
                     currentList[pc].row = pc - (currentList.length / 2)
                     offset = (currentList.length / 2);
-                    console.log("first offset: " + offset)
                 }
                 else {
                     currentList[pc].row = offset;
                     offset++;
-                    console.log(c);
-                    console.log("later offset: " + offset)
                 }
                 }
                 isFirst = false;
@@ -194,72 +230,4 @@ d3_layout_bloomcase = function() {
     }
     }
 
-}
-
-function testThis() {
-    	var shapeMeasures = {
-	    Inspiration: {myWidth: 27, myHeight: 27,
-	    pathd: "M24.248,26.755H2.833c-1.404,0-2.548-1.143-2.548-2.548V2.793c0-1.405,1.144-2.548,2.548-2.548h21.415 c1.405,0,2.549,1.143,2.549,2.548v21.414C26.796,25.612,25.653,26.755,24.248,26.755z"                            },
-	    Final: {myWidth: 47.4, myHeight: 47.4,
-	    pathd: "M47.392,44.295c0,1.709-1.388,3.096-3.097,3.096H3.096C1.386,47.391,0,46.004,0,44.295V3.096 C0,1.386,1.386,0,3.096,0h41.199c1.709,0,3.097,1.386,3.097,3.096V44.295z"
-	    },
-	    Draft: {myWidth: 35, myHeight: 34.8,
-            pathd: "M31.42,34.758H3.341C1.5,34.758,0,33.26,0,31.419V3.341C0,1.499,1.5,0,3.341,0H31.42 c1.843,0,3.342,1.499,3.342,3.341v28.079C34.762,33.26,33.263,34.758,31.42,34.758z"
-	    },
-	    Sketch: {myWidth: 26.5, myHeight: 26.5,
-	    pathd: "M23.963,26.51H2.548C1.143,26.51,0,25.367,0,23.962V2.548C0,1.143,1.143,0,2.548,0h21.415 c1.405,0,2.549,1.143,2.549,2.548v21.414C26.511,25.367,25.368,26.51,23.963,26.51z"
-	    },
-	    Idea: {myWidth: 33.8, myHeight: 38.6,
-	    pathd: "M16.897,0C14.483,7.241,7.241,16.896,0,19.311c7.241,2.414,14.483,12.069,16.897,19.312 c2.414-7.241,9.654-16.896,16.896-19.312C26.551,16.896,19.311,7.241,16.897,0z"
-	    }
-	}
-
-    newNodes = {};
-    testLayout = new d3_layout_bloomcase();
-    d3.json("new8.json", function(data) {newNodes = data;
-    
-    testLayout.nodes(newNodes.nodes);
-
-    d3.select("#bloomG").selectAll("line").data(testLayout.links()).enter().append("line")
-    .attr("x1", 30)
-    .attr("x2", 30)
-    .attr("y1", function(d,i) {return 200 + (d.source.row * 40)})
-    .attr("y2", function(d,i) {return 200 + (d.target.row * 40)})
-    .style("stroke", "black")
-    .style("stroke-width", 2)
-    .transition()
-    .duration(1000)
-    .attr("x1", function(d,i) {return d.source.column * 30})
-    .attr("x2", function(d,i) {return d.target.column * 30}) 
-    .attr("y1", function(d,i) {return 200 + (d.source.row * 40)})
-    .attr("y2", function(d,i) {return 200 + (d.target.row * 40)})
-
-
-    
-    var secG = d3.select("#bloomG").selectAll("g.sec").data(testLayout.nodes()).enter().append("g")
-    .attr("width", 20)
-    .attr("height", 20)
-    .attr("transform", function(d,i) {return "translate(30,"+ (200 + (d.row * 40)) +")"})
-    
-    secG.append("path")
-    .attr("class", function(d){ return d.kind; })
-    .attr("d", function(d,i) {return shapeMeasures[d.kind]["pathd"]})
-    .attr("transform", function(d) { 
-	return "translate(" + (-1*(shapeMeasures[d.kind]["myWidth"]/2)) + "," + (-1*(shapeMeasures[d.kind]["myHeight"]/2)) + ")"; 
-    });
-
-    secG.append("text")
-                .attr("dx", -1)
-                .attr("dy", ".35em")
-                .attr("alignment-baseline", "center")
-                .attr("text-anchor", "middle")
-                .style("fill", "white")
-                .text(function(d) { return d.nid });
-    
-    secG.transition().duration(1000)
-    .attr("transform", function(d,i) {return "translate("+ (d.column * 30) +","+ (200 + (d.row * 40)) +")"})
-    
-
-    });
-    
 }
